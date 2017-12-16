79b755d54027540ef3548d6323daed0d43b5d14d
hex4 <VikramBharadwaj1995@users.noreply.github.com>
2017-12-16 23:28:03 +0530

    Add files via upload

---
+++ TweetAnalysis.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 16 20:18:57 2017
+
+@author: viksr
+"""
+# This is a Simple Aplication that classifies a Tweet as a Positive one or a Negative one.
+
+import tweepy
+from textblob import TextBlob
+
+consumerKey = #Enter your key
+consumerKeySecret = #Enter your key
+
+accessToken = #Enter your key
+accessTokenSecret = #Enter your key
+
+loginAuth = tweepy.OAuthHandler(consumerKey, consumerKeySecret)
+loginAuth.set_access_token(accessToken, accessTokenSecret)
+
+TwitterApi = tweepy.API(loginAuth)
+
+print("Enter a particular Search Term/Query, to get an Analysis on the Polarity and Subjectivity of a tweet with that term!")
+inputQuery = input()
+publicTweets = TwitterApi.search(inputQuery)
+
+for tweet in publicTweets:
+    print(tweet.text)
+    textTweet = tweet.text
+    tweetAanalysis = TextBlob(textTweet)
+    print(tweetAanalysis.sentiment)
+    print("\n")
+    
